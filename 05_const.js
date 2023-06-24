5f45b11dc1b171da078efc1a6e59f1642ad3c59b
Huzaifa Khan <hk7928042@gmail.com>
2023-06-24 22:30:27 +0500

    const done Allhamdullillah

---
+++ 05_const.js
@@ -0,0 +1,9 @@
+// Const are not change able throughout the program:
+
+const author = 'Huzaifa';
+
+// let author = 23;     This is not allowed to change any const variable.
+// author = 23;         This is also not allowed.
+// const author;        This is also not allowed. You must initialize const.
+
+console.log(author);
