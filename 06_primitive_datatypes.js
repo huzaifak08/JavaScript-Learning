7f66bed8002bd43c5a6fe97bc7e92c5d64ca4bc6
Huzaifa Khan <hk7928042@gmail.com>
2023-06-24 23:11:41 +0500

    Primitive and nonprimitive datatypes done Allhamdullillah

---
+++ 06_primitive_datatypes.js
@@ -0,0 +1,15 @@
+// nn bb ss u
+let n1 = null;              // Null
+let n2 = 345;               // Number
+
+let b1 = true;              // Boolean
+let b2 = BigInt("567");     // BigInt
+
+let s1 = 'String';          // String
+let s2 = Symbol('I am a nice symbol');      // Symbol
+
+let u = undefined;          // Undefined
+
+console.log(n1, n2, b1, b2, s1, s2, u);
+
+console.log(typeof b2);
---
+++ 07_objects.js
@@ -0,0 +1,9 @@
+// Objects are non primitive datatypes: Key Value Pairs
+
+const item = {
+    'Huzaifa': true,
+    'Hanzla': 17,
+    'Kamran': undefined,
+}
+
+console.log(item['Hanzla']);
