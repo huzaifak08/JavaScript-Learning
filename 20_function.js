08d320191a0892949e6644d281c87bff560ce73e
Huzaifa Khan <hk7928042@gmail.com>
2023-06-25 22:22:28 +0500

    Functions done Allhamdullillah

---
+++ 20_function.js
@@ -0,0 +1,15 @@
+// Function:
+
+// Simple Way:
+function average(a, b) {
+
+    return (a + b) / 2;
+}
+
+// Fancy Way: Arrow Function.
+const sum = (a, b) => {
+    return a + b;
+}
+
+console.log('Average Function Output is: ', average(12, 10));
+console.log('Sum Function Output is: ', sum(12, 10));
