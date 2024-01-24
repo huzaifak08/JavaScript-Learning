0c3863b19b69831a690fd4c76e4c0a39a7f8f385
huzaifak08 <hk7928042@gmail.com>
2024-01-24 17:43:52 +0500

    Table navigation done Allhamdullillah

---
+++ 77_table_navigation/script.js
@@ -0,0 +1,11 @@
+let t = document.body.firstElementChild.firstElementChild;
+
+console.log(t);
+console.log(t.rows);
+console.log(t.caption);
+console.log(t.tHead);
+console.log(t.tHead.firstElementChild);
+console.log(t.tFoot);
+console.log(t.tBodies);
+console.log(t.rows[0].cells);
+console.log(t.rows[0].rowIndex);
